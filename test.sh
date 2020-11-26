a66c9091c6656c406f8030292f790f145a120ec3
Calx822 <sahinfd822@gmail.com>
2020-11-26 20:49:29 +0200

    pushing files

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --account=project_2000924
+#SBATCH --partition=gpu
+#SBATCH --time=00:15:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=1000
+#SBATCH --gres=gpu:v100:1
+
+module load pytorch/1.6
+
+srun python3 /users/student104/examples/mnist/main.py
